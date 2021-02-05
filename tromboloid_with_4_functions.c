96579ea7bf470fc3e70d9f4ca96e9a335d981ff5
Rubeensakeena23 <75937165+Rubeensakeena23@users.noreply.github.com>
2021-02-05 17:47:00 +0530

    Update tromboloid_with_4_functions.c

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -1,1 +1,26 @@
 //WAP to find the volume of a tromboloid using 4 functions.
+#include<stdio.h>
+int input()
+{
+  int a;
+  scanf("%d", &a);
+  return a;
+}
+void output()
+{
+  printf("The sum is : %d \n", Sum());
+}
+int Sum(int a, int b)
+{
+  int sum = a+ b;
+  return sum;
+}
+void main()
+{
+  printf("Enter a: ");
+  int a = input();
+  printf("Enter b: ");
+  int b = input();
+  Sum(a,b);
+  output();
+}
