7c65ef6c9fc7ef3a8f05af9f8f30d50f81cd6523
Rubeensakeena23 <75937165+Rubeensakeena23@users.noreply.github.com>
2021-02-17 09:55:45 +0530

    Update tromboloid_with_1_function.c

--- tromboloid_with_1_function.c
+++ tromboloid_with_1_function.c
@@ -2,15 +2,15 @@
 #include<stdio.h>
 int main()
 {
-int a,b,c;
-float vol;
-printf("Enter a:");
-scanf("%d", &a);
-printf("Enter b:");
-scanf("%d", &b);
-printf("Enter c:");
-scanf("%d", &c);
-vol= ((a*c*b) + (c/b))/3;
-printf(" The volume of the Tromboloid is %f",vol);
-return 0;
+  int a,b,c;
+  float vol;
+  printf("Enter a:");
+  scanf("%d", &a);
+  printf("Enter b:");
+  scanf("%d", &b);
+  printf("Enter c:");
+  scanf("%d", &c);
+  vol= ((a*c*b) + (c/b))/3;
+  printf(" The volume of the Tromboloid is %f",vol);
+  return 0;
 }
