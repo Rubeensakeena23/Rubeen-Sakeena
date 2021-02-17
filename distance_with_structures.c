890e4730d40eea3d0aa787af97c6152a3afb4681
Rubeensakeena23 <75937165+Rubeensakeena23@users.noreply.github.com>
2021-02-17 10:17:19 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -5,31 +5,31 @@ int x,y;
 };
 struct points input1(struct points a)
 {
-printf("Enter the first coordinate");
-scanf("%d %d", &a.x, &a.y);
-return a;
+    printf("Enter the first coordinate");
+    scanf("%d %d", &a.x, &a.y);
+    return a;
 }
 struct points input2(struct points b)
 {
-printf("Enter the second coordinate");
-scanf("%d %d", &b.x, &b.y);
-return b;
+    printf("Enter the second coordinate");
+    scanf("%d %d", &b.x, &b.y);
+    return b;
 }
 float distance(struct points a, struct points b)
 {
-float d;
-d = sqrt(((b.x-a.x)*(b.x-a.x))+((b.y-a.y)*(b.y-a.y)));
-return d;
+    float d;
+    d = sqrt(((b.x-a.x)*(b.x-a.x))+((b.y-a.y)*(b.y-a.y)));
+    return d;
 } 
 void output(struct points a, struct points b)
 {
-printf("The distance between (%d,%d) and (%d,%d) is : %f", a.x,a.y,b.x,b.y,distance(a,b));
+    printf("The distance between (%d,%d) and (%d,%d) is : %f", a.x,a.y,b.x,b.y,distance(a,b));
 } 
 int main()
 {
-struct points a, b;
-a = input1(a);
-b = input2(b);
-output(a,b);
-return 0;
+    struct points a, b;
+    a = input1(a);
+    b = input2(b);
+    output(a,b);
+    return 0;
 }
